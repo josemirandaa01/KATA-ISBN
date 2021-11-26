0b66a61dbbffafc405b9ba8ea5c3338938511cd6
jose <josemirandaalmeyda@hotmail.com>
2021-11-26 17:30:34 -0400

    version final

--- Prueba ISBN/ISBN.cs
+++ Prueba ISBN/ISBN.cs
@@ -5,18 +5,82 @@ namespace ISBN
 
     class Program
     {
+        
 
-        public static string ISBN(long n)
+        public static string ISBN10(long n)
         {
-            string cifras = "";
-            int a = 5, b = 2;
 
-            cifras = cifras + b;
-            cifras = cifras + a;
-            Console.WriteLine(cifras);
+        long[] cifras = new long[10];
+        long[] mult = new long[10];
+
+            int i = 9;
+            int b = 1;
+            long suma = 0;
+
+            while (n != 0)
+        {
+            long a;
+            a = n % 10;
+            n = n / 10;
+            cifras[i] = a;
+            mult[i] = a * b;
+            suma = suma + mult[i];
+            i--;
+            b++;  
+        }
+
+            suma = suma % 11;
+
+            if (suma == 0)
+            {
+                return "ISBN válido";
+            }
+            
+        return "ISBN inválido";
+        }
+
+        public static string ISBN13(long n)
+        {
+
+            long[] cifras = new long[13];
+            long[] mult = new long[13];
+
+            int i = 12;
+            int b = 1;
+            long suma = 0;
+            bool par = false;
+
+            while (n != 0)
+            {
+                long a;
+                a = n % 10;
+                n = n / 10;
+                cifras[i] = a;
+                if(par == false)
+                {
+                    mult[i] = a * 1;
+                    par = true;
+                }
+                else if (par == true)
+                {
+                    mult[i] = a * 3;
+                    par = false;
+                }
+
+                suma = suma + mult[i];
+                i--;
+                b++;
+            }
+
+             suma = suma % 10;
+
+            if (suma == 0)
+            {
+                return "ISBN válido";
+            }
             
 
-            return "ISBN válido";
+            return "ISBN inválido";
         }
 
     }
--- Prueba ISBN/UnitTest1.cs
+++ Prueba ISBN/UnitTest1.cs
@@ -7,26 +7,27 @@ namespace Prueba_ISBN
 
         /*
         Caso1: ISBN 1234567891234 -> ISBN válido | ISBN de 13 dígitos
-        Caso2: ISBN 1234567890 -> ISBN válido | ISBN de 10 dígitos
+        Caso2: ISBN 0716703440 -> ISBN válido | ISBN de 10 dígitos
         Caso3: ISBN 123456789 -> ISBN inválido | ISBN menor de 10 dígitos
         Caso4: ISBN 123456789012 -> ISBN inválido | ISBN mayor de 10 dígitos pero menor de 13 dígitos
         */
 
+
         [Test]
         public void Caso1()
         {
-            long value = 1234567891234;
+            long value = 0716703440;
             string expected = "ISBN válido";
-            string result = ISBN.Program.ISBN(value);
+            string result = ISBN.Program.ISBN10(value);
             Assert.AreEqual(expected, result);
         }
 
         [Test]
         public void Caso2()
         {
-            long value = 1234567890;
+            long value = 9780716703440;
             string expected = "ISBN válido";
-            string result = ISBN.Program.ISBN(value);
+            string result = ISBN.Program.ISBN13(value);
             Assert.AreEqual(expected, result);
         }
 
@@ -35,7 +36,7 @@ namespace Prueba_ISBN
         {
             long value = 123456789;
             string expected = "ISBN inválido";
-            string result = ISBN.Program.ISBN(value);
+            string result = ISBN.Program.ISBN10(value);
             Assert.AreEqual(expected, result);
         }
 
@@ -44,8 +45,27 @@ namespace Prueba_ISBN
         {
             long value = 123456789012;
             string expected = "ISBN inválido";
-            string result = ISBN.Program.ISBN(value);
+            string result = ISBN.Program.ISBN10(value);
             Assert.AreEqual(expected, result);
         }
+
+        [Test]
+        public void Caso5()
+        {
+            long value = 54;
+            string expected = "45";
+            string result = ISBN.Program.ISBN10(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Caso6()
+        {
+            long value = 0716703440;
+            string expected = "0";
+            string result = ISBN.Program.ISBN10(value);
+            Assert.AreEqual(expected, result);
+        }
+
     }
 }
